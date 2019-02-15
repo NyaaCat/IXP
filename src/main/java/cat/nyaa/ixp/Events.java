d0d3ceedf4eb22352eed5e2624dd559dd1298352
ReinWD <ReinWDD@gmail.com>
2019-02-15 20:56:04 +0800

    change class name in order to prevent unnecessary qualification change IXP Sign judge method

--- src/main/java/cat/nyaa/ixp/Events.java
+++ src/main/java/cat/nyaa/ixp/Events.java
@@ -1,6 +1,7 @@
 package cat.nyaa.ixp;
 
 import cat.nyaa.ixp.sign.BaseSign;
+import cat.nyaa.ixp.sign.SignManager;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
@@ -9,25 +10,28 @@ import org.bukkit.block.data.BlockData;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
+import org.bukkit.event.block.SignChangeEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 
 import java.util.HashMap;
 import java.util.Map;
 
+import org.bukkit.event.EventHandler;
+
 /**
  * @author ReinWD
  * created @ 2019/2/4
  */
-public class EventHandler implements Listener {
+public class Events implements Listener {
     private IXPPlugin plugin;
     private Map<Location, BaseSign> signMap;
 
-    EventHandler(IXPPlugin plugin){
+    Events(IXPPlugin plugin){
         this.plugin = plugin;
         signMap = new HashMap<>();
     }
 
-    @org.bukkit.event.EventHandler
+    @EventHandler
     public void onClickSign(PlayerInteractEvent event){
         Action action = event.getAction();
         if (action.equals(Action.RIGHT_CLICK_BLOCK) && event.getClickedBlock().getBlockData().getMaterial().equals(Material.WALL_SIGN)) {
@@ -46,7 +50,8 @@ public class EventHandler implements Listener {
     }
 
     private boolean isIXPSign(Sign sign) {
-        return "[ixp]".equalsIgnoreCase(sign.getLine(0)) &&
-                ("SEND".equalsIgnoreCase(sign.getLine(1)) || "RECEIVE".equalsIgnoreCase(sign.getLine(1)));
+//        return "[ixp]".equalsIgnoreCase(sign.getLine(0)) &&
+//                ("SEND".equalsIgnoreCase(sign.getLine(1)) || "RECEIVE".equalsIgnoreCase(sign.getLine(1)));
+        return SignManager.getInstance().hasSignAt(sign.getBlock().getLocation());
     }
 }
